19658bfca1560fb56522291714ded87912117e16
Alex-Colgan <alexandercolgan@gmail.com>
2022-09-28 16:43:13 +0100

    16:43 28-09-22

--- 27-09-22 Session 13/script.js
+++ 27-09-22 Session 13/script.js
@@ -27,10 +27,74 @@ function addItem() {
 
 submit.onclick = addItem;*/
 
-var item = document.getElementById("item").value;
-var list = document.createElement("ul");
-function addItemFunction() {
-list.innerHTML = item;
-list.appendChild(list);
-item.value ="";
+
+
+/*function validationForm() {
+    console.log("I am in the validation")
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        console.log("I am in the validation")
+        return false
+    } else {
+        alert("Name was filled out and would go to post!")
+        }
+}*/
+
+//Creating variables for our HTML tags and an empty array that will be our final list.
+let list = [];
+let shoppingList = document.getElementById("shoppingList");
+let input = document.getElementById("item");
+let add = document.getElementById("add");
+
+//Creating javascript for our HTML button that runs the addItem function when it is clicked.
+const btn = document.getElementById('add')
+btn.addEventListener('click', addItem)
+
+function addItem() {
+
+    //New local variable for our new items. Variables added in a function are local to that function.
+    let newItem = input.value;
+
+    //If else statements give info to user if input field is used incorrectly.
+    if (list.includes(newItem)) {
+        alert("That has already been added.");
+    } else if (newItem == "") {
+        alert ("Field cannot be left blank.");
+    } else {
+
+        //Push puts our new item at the end of our list.
+        list.push(newItem);
+
+        //Empties our input variable for it's next use.
+        input.value = "";
+
+        //Console log for testing.
+        console.log(list);
+
+        /**This is creating a new variable and 
+         * asigning it to the element with the id of li from the html page.
+         */
+        let newLine = document.getElementById("li");
+
+        /**This line seems to just be creating a new element called p and
+         * asigning the content of it to the new variable item. 
+         * But if I remove this the function doesn't work properly.
+         * Maybe it's creating a paragragh tag in HTML but I don't think so.
+         * I don't know why this works.
+         */
+        let item = document.createElement("p");
+
+        //Puts the text content of newItem into item.
+        item.textContent = newItem;
+
+        //Adds a new line to the end of our item so our list items don't all appear on the same line.
+        newLine.append(item);
+
+        //Failed coding attempts trying to get the list items to appear on a new line.
+        //newLine.append("\n");
+        //'-'+ newItem + '<br/>'
+        //shoppingList.append(newLine);
+        //`Already added'${items[i]}`
+    }
 }
