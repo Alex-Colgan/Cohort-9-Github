4dc6a7c8be3410637cdb92a5be35465b0f1fe4cd
Alex-Colgan <alexandercolgan@gmail.com>
2022-10-22 18:58:34 +0100

    Frontend mentor challenge complete

--- 20-10-22 Session 22/interactive-card-details-form-main/script.js
+++ 20-10-22 Session 22/interactive-card-details-form-main/script.js
@@ -1,16 +1,52 @@
 // TO DO: change the identifying names to fit your code
 
 // Update the deatils on the card as the user fills in the fields.
-const form = document.getElementById('checkout-form');
-const cardholderNameInput = document.getelementbyid('cname');
+//const form = document.getElementById('checkout-form');
+//const cardholderNameInput = document.getElementByid('cname');
 
 
 
 // Console log the cardholder name when you type in the input.
 // When input changes update teh card text on the card display using innerhtml.
-function updateCardDetails() {
+//function updateCardDetails() {
    // console.log(cardholderNameInput.value);
-   document.getElementById('card_cname').innerHTML = cardholderNameInput.value;
+   //document.getElementById('card_cname').innerHTML = cardholderNameInput.value;
+//}
+//cardholderNameInput.addEventListener('change', updateCardDetails);
+
+
+
+let submit = document.getElementById('submit');
+submit.addEventListener('click', (e) => {
+   console.log(e);
+   let name = e.path[1][0].value;
+   console.log(name);
+   let number = e.path[1][1].value;
+   console.log(number);
+   let mm = e.path[1][2].value;
+   let yy = e.path[1][3].value;
+   let cvc = e.path[1][4].value;
+
+   if(  name === '' 
+  || number=== ''
+  || mm === ''
+  || yy ===  ''
+  || cvc === ''
+  
+  ){
+    alert('please fill in all sections')
+  } else {
+
+
+   let cardNumberEg = document.getElementById('cardNumberEg');
+   let cardNameEg = document.getElementById('cardNameEg');
+   let cardDateEg = document.getElementById('cardDateEg');
+   let cardCVCEg = document.getElementById('cardCVCEg');
+
+   cardNumberEg.innerHTML = number;
+   cardNameEg.innerHTML = name;
+   cardDateEg.innerHTML = mm + "/" + yy;
+   cardCVCEg.innerHTML = cvc;
 }
-cardholderNameInput.addEventListener('change', updateCardDetails);
+});
 
