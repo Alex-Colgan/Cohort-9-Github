2c4bdbf2e0b8d3441610ed7531221708e0855691
Alex-Colgan <alexandercolgan@gmail.com>
2022-10-24 14:26:16 +0100

    Introduction to React

---
+++ 24-10-22 Session 24/like_button.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    /** Conditional that says if the state is "liked" then return a string: you liked this. */
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
